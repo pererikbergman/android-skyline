0c5ef9fbae02c85de63d61cf55a35770b09eb7c8
Per-Erik Bergman <bergman@uncle.se>
2017-03-27 20:24:38 +0700

    Bumped version.

--- app/src/main/java/com/rakangsoftware/skyline/sample/screen/demo/DemoActivity.java
+++ app/src/main/java/com/rakangsoftware/skyline/sample/screen/demo/DemoActivity.java
@@ -26,10 +26,6 @@ import com.rakangsoftware.skyline.sample.R;
 
 public class DemoActivity extends AppCompatActivity {
 
-    public static void start(Context context) {
-        context.startActivity(new Intent(context, DemoActivity.class));
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,5 +42,9 @@ public class DemoActivity extends AppCompatActivity {
                 .beginTransaction()
                 .replace(R.id.fragment_container, fragment)
                 .commit();
+    }
+
+    public static void start(Context context) {
+        context.startActivity(new Intent(context, DemoActivity.class));
     }
 }
